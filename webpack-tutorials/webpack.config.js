cdd203159f06ce74e1d6652d1260870dfe71d2fc
Derek <harrity-development@gmail.com>
2017-10-05 00:18:45 -0500

    split configs into main cnofig and parts.config

--- webpack-tutorials/webpack.config.js
+++ webpack-tutorials/webpack.config.js
@@ -1,59 +1,46 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
 
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build'),
 };
 
-const commonConfigs = {
-    entry: {
-        app: PATHS.app,
-    },
-
-    output: {
-        path: PATHS.build,
-        filename: '[name].js',
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack Demo',
-        }),
-    ],
-};
-
-const productionConfigs = () => commonConfigs;
-
-const developmentConfigs = () => {
-    const config = {
-        devServer: {
-            historyApiFallback: true,
-            stats: 'errors-only',
-
-            // Parse host and port from env to allow customization.
-            //
-            // If you use Docker, Vagrant or Cloud9, set
-            // host: options.host || '0.0.0.0';
-            //        
-            // 0.0.0.0 is available to all network devices
-            // unlike default `localhost`.
-            host: process.env.host, //defaults to 'localhost',
-            port: process.env.port, //defaults to '8080'
+const commonConfigs = merge([
+    {
+        entry: {
+            app: PATHS.app,
         },
-    };
+        output: {
+            path: PATHS.build,
+            filename: '[name].js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'webpack demo',
+            }),
+        ],
+    },
+    parts.lintJavaScript({ include: PATHS.app }),
+]);
 
-    return Object.assign(
-        {}, 
-        commonConfigs, 
-        config
-    );
-};
+const productionConfigs = merge([]);
+
+const developmentConfigs = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+]);
 
 module.exports = (env) => {
     if (env === 'production'){
-        return productionConfigs();
+        return merge(commonConfigs, productionConfigs);
     }
-
-    return developmentConfigs();
+    
+    return merge(commonConfigs, developmentConfigs);
 };
